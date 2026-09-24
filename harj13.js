61a0d4437665f7560118bf35d51bf5347aca1a48
hploompuu-sketch <hannesploompuu@gmail.com>
2026-09-24 16:26:04 +0300

    Parem

--- harj13.js
+++ harj13.js
@@ -1,10 +1,26 @@
 const kaardid =document.querySelectorAll(".card")
+const pealkiri =document.querySelectorAll("h5");
+const kirj =document.querySelectorAll("p");
+const loend =document.querySelectorAll("li");
+
+loend.forEach(e => {
+    if(e.textContent.includes ("Tehtud")) {
+        e.classList.add("list-group-item-warning")
+    }
+});
+
+
+
+let m = 0
 
 kaardid.forEach(kaart =>{
-    const pealkiri =document.querySelectorAll("h5")
+    
     let nimi = (kaart.querySelector("img").getAttribute("data-title"));
     let kirjeldus = (kaart.querySelector("img").getAttribute("data-description"));
-    console.log(kirjeldus, nimi)
+    pealkiri[m].innerHTML=nimi
+    kirj[m].innerHTML = kirjeldus
+    m+=1
+    // console.log(nimi)
 });
 
 
